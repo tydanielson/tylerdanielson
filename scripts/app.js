794b4bb96882101d9fa824a92ef27e66e201c339
Tyler Danielson <tysdanielson@gmail.com>
2016-06-06 13:04:52 -0500

    update with latest job and projects

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,15 @@
-(function(){
+/*global angular */
+(function () {
+    'use strict';
 
-var app = angular.module('TyDanielson', ['ngMaterial'])
-    .config(function($mdThemingProvider) {
-        $mdThemingProvider.theme('default')
-            .primaryPalette('green')
-            .accentPalette('blue');
+    angular.module('TyDanielson', ['ngMaterial'])
+        .config(function ($mdThemingProvider) {
+            $mdThemingProvider.theme('default')
+                .primaryPalette('green')
+                .accentPalette('blue');
 
-});
-
-app.controller('AppCtrl', ['$scope', function($scope){
+        }).controller('AppCtrl', ['$scope', function ($scope) {
 
 
-}]);
-})();
+        }]);
+}());
